033fb7ddaf145f09a6b75d5a21d1e3827c7e3ce8
hyeri <shinhro3o@gmail.com>
2023-07-26 11:30:01 +0900

    feat: 02. TypeScript에서 Array와 Tuple의 데이터 타입 내용 추가

---
+++ src/02/ts02.ts
@@ -0,0 +1,9 @@
+//Array 타입
+//첫 번째 선언 방식
+let arr1: number[] = [1, 2, 3];
+
+//두 번째 선언 방식
+let arr2: Array<number> = [1, 2, 3];
+
+//Tuple 타입
+let tuple: [string, number, boolean] = ["Hello", 42, true];
